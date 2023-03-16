6f01439d9e629f30f0237e9987bdb4543704ac61
mariam <mariam@MYPROBOOK>
2023-03-16 17:05:33 +0400

    Add project files.

---
+++ AbstractClass/Father.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Father
+    {
+        public void Eyecolor() 
+        {
+            Console.WriteLine("Fathers eyes are blue.");
+        }
+        public abstract void Surname();
+    }
+    class Son:Father
+    {
+        public void Name(string name)
+        {            
+            Console.WriteLine($"Son's name is {name}.");
+        }
+        
+
+            public override void Surname()
+        {
+            Console.WriteLine("Enter surname: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine($"Their surname is {surname}.");
+        }        
+    }
+}
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Father father = new Son();
+            father.Surname();
+            father.Eyecolor();
+
+            Son son = new Son();
+            son.Name("Tom");
+        }
+    }
+}
